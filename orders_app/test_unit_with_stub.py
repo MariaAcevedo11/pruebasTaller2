7e126a61c81f85518ec88ab416e0fcbe16160856
MariaAcevedo11 <macevedos2@eafit.edu.co>
2026-04-06 19:08:56 -0500

    Taller2

---
+++ orders_app/test_unit_with_stub.py
@@ -0,0 +1,26 @@
+from order_service import create_order
+from database import SessionLocal
+from models import Order
+
+class StubUserRepository:
+    def get_user_email(self, user_id):
+        user_id="mariaStub@gmail.com"
+        return user_id
+
+class DummyLogger:
+    def log(self, msg):
+        pass
+
+class NullNotifier:
+    def send(self, to, message):
+        pass
+
+def test_create_order_with_stub():
+    db = SessionLocal()
+    order = create_order(10, 200, NullNotifier(), DummyLogger(), db, StubUserRepository())
+    assert order.status == 'CREATED'
+    assert order.user_email == "mariaStub@gmail.com"
+    
+    db.query(Order).delete()
+    db.commit()
+    db.close()
